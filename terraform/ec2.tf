a2e0c8827cbbadc33370d89c37e88a6bb8485180
Peimli Zoltán <zoltanpeimli23@gmail.com>
2025-04-08 10:29:37 +0200

    Changed instance types and region.

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -57,7 +57,7 @@ resource "aws_security_group" "main" {
 
 resource "aws_instance" "web" {
   ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t2.micro"
+  instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.public_1.id
   key_name                    = aws_key_pair.deployer.key_name
   vpc_security_group_ids      = [aws_security_group.main.id]
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,18 @@
 variable "aws_region" {
   description = "The AWS region to deploy resources in"
-  default     = "eu-central-1"
+  default     = "us-east-1"
+}
+
+variable "db_name" {
+  description = "Name of the database"
+  type        = string
+  default     = "serpent_surge_db"
+}
+
+variable "db_username" {
+  description = "Master username for the database"
+  type        = string
+  default     = "admin"
 }
 
 variable "db_password" {
